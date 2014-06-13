44321e2758e47f755cfaffe07b2402621bfb7aa3
alex <alex@deb-seven-two>
2014-06-13 09:13:00 -0700

    change

---
+++ src/glue/generic/glue/socket.h
@@ -0,0 +1,101 @@
+#ifndef _SOCKET_H_tcpstriper_
+#define _SOCKET_H_tcpstriper_
+
+/*
+ *	Posix-ish Socket API
+ *
+ * 		-- constants --
+ *
+ * 		AF_INET, SOCK_STREAM, SOCK_DGRAM
+ *
+ * 		SOL_SOCKET
+ * 			SO_ERROR         int
+ * 			SO_RCVBUF        int
+ * 			SO_SNDBUF        int
+ * 			SO_REUSEADDR     int
+ *			SO_LINGER        linger
+ *
+ * 		IPPROTO_IP
+ * 			IP_TTL           int
+ *
+ * 		IPPROTO_TCP
+ * 			TCP_NODELAY      int
+ *
+ * 		IPPROTO_UDP
+ * 			<none>
+ *
+ * 		-- types --
+ * 		
+ *		int (not socket_t) for a socket descriptor
+ *
+ *		socklen_t
+ * 		sockaddr
+ * 		sockaddr_in
+ *		linger
+ *
+ *		ip4_addr_t
+ *
+ * 		-- functions --
+ *
+ *		int sk_create(int family, int type, int protocol);
+ *		int sk_shutdown(int sk);   // SD_WRITE, i.e. send FIN
+ *		int sk_close(int sk);
+ *
+ *		int sk_bind(int sk, const sockaddr * addr, socklen_t alen);
+ *
+ *		int sk_connect(int sk, const sockaddr * a, socklen_t alen); 
+ *
+ *		int sk_listen(int sk, int backlog);
+ *		int sk_accept(int sk, sockaddr * addr, socklen_t alen);
+ *
+ *		int sk_getsockname(int sk, sockaddr * a, socklen_t alen);
+ *		int sk_getpeername(int sk, sockaddr * a, socklen_t alen);
+ *		
+ *		int sk_recv(int sk, void * p, size_t n, 
+ *		            sockaddr * src = NULL, socklen_t alen = 0);
+ *		int sk_send(int sk, const void * p, size_t n, 
+ *		            sockaddr * dst = NULL, socklen_t alen = 0);
+ *
+ *		int sk_getsockopt(int sk, int level, int opt, 
+ *		                  void * val, socklen_t vlen);
+ *		int sk_setsockopt(int sk, int level, int opt, 
+ *		                  const void * val, socklen_t vlen);
+ *
+ * 	Non-Posix extensions:
+ *
+ *		int sk_unblock(int sk);
+ *
+ *		int sk_errno();       // aka "fast", errno 
+ *		int sk_error(int sk); // aka "slow", getsockopt(so_error)
+ *
+ * 		int sk_conn_fatal(int err); // boolean return
+ * 		int sk_recv_fatal(int err);
+ * 		int sk_send_fatal(int err);
+ *
+ *		int sk_conn_refused(int err);
+ *		int sk_conn_timeout(int err);
+ *
+ * 		void  sockaddr_in_init(sockaddr_in &);
+ *
+ *		SOCKADDR_IN_ADDR(sa)
+ *		SOCKADDR_IN_PORT(sa)
+ *
+ * 		htons, ntohs, htons_const
+ * 		htonl, ntohl, htonl_const
+ *
+ *	Notes:
+ *
+ *		On windows - if the connection is aborted by peer (e.g. 
+ *		via linger/close), and there's unread data in a socket 
+ *		buffer, the socket will show as 'readable' when polled 
+ *		for status, but recv() will fail with ECONNRESET.
+ *
+ *		So for the sake of portability, it should be assumed that 
+ *		if a socket enters a 'reset' (or 'error') state, all its 
+ *		buffers - both inbound and outbound - are cleared by the OS.
+ */
+
+#error Set your Include paths to use platform-specific version of this file
+
+#endif
+
---
+++ src/glue/linux/glue/socket.h
@@ -0,0 +1,257 @@
+#ifndef _SOCKET_H_linux_tcpstriper_
+#define _SOCKET_H_linux_tcpstriper_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+/*
+ *	Posix-ish Socket API
+ *
+ * 		-- constants --
+ *
+ * 		AF_INET, SOCK_STREAM, SOCK_DGRAM
+ *
+ * 		SOL_SOCKET
+ * 			SO_ERROR         int
+ * 			SO_RCVBUF        int
+ * 			SO_SNDBUF        int
+ * 			SO_REUSEADDR     int
+ *			SO_LINGER        linger
+ *
+ * 		IPPROTO_IP
+ * 			IP_TTL           int
+ *
+ * 		IPPROTO_TCP
+ * 			TCP_NODELAY      int
+ *
+ * 		IPPROTO_UDP
+ * 			<none>
+ *
+ * 		-- types --
+ * 		
+ *		int (not socket_t) for a socket descriptor
+ *
+ *		socklen_t
+ * 		sockaddr
+ * 		sockaddr_in
+ *		linger
+ *
+ *		ip4_addr_t
+ *
+ * 		-- functions --
+ *
+ *		int sk_create(int family, int type, int protocol);
+ *		int sk_shutdown(int sk);   // SD_WRITE, i.e. send FIN
+ *		int sk_close(int sk);
+ */
+
+inline
+int sk_create(int family, int type, int protocol)
+{
+	return socket(family, type, protocol);
+}
+
+inline
+int sk_shutdown(int sk)
+{
+	return shutdown(sk, SHUT_WR);
+}
+
+inline
+int sk_close(int sk)
+{
+	return close(sk);
+}
+
+ /*
+  *		int sk_bind(int sk, const sockaddr * addr, socklen_t alen);
+  *
+  *		int sk_connect(int sk, const sockaddr * a, socklen_t alen); 
+  *
+  *		int sk_listen(int sk, int backlog);
+  *		int sk_accept(int sk, sockaddr * addr, socklen_t alen);
+  */
+
+inline
+int sk_bind(int sk, const sockaddr * addr, socklen_t alen)
+{
+	return bind(sk, addr, alen);
+}
+
+inline
+int sk_connect(int sk, const sockaddr * a, socklen_t alen)
+{
+	return connect(sk, a, alen);
+}
+
+inline
+int sk_listen(int sk, int backlog)
+{
+	return listen(sk, backlog);
+}
+
+inline
+int sk_accept(int sk, sockaddr * addr, socklen_t alen)
+{
+	return accept(sk, addr, &alen);
+}
+
+ /*
+  *		int sk_getsockname(int sk, sockaddr * a, socklen_t alen);
+  *		int sk_getpeername(int sk, sockaddr * a, socklen_t alen);
+  */
+
+inline
+int sk_getsockname(int sk, sockaddr * a, socklen_t alen)
+{
+	return getsockname(sk, a, &alen);
+}
+
+inline
+int sk_getpeername(int sk, sockaddr * a, socklen_t alen)
+{
+	return getpeername(sk, a, &alen);
+}
+
+/*
+ *		int sk_recv(int sk, void * p, size_t n, 
+ *		            sockaddr * src = NULL, socklen_t alen = 0);
+ *		int sk_send(int sk, const void * p, size_t n, 
+ *		            sockaddr * dst = NULL, socklen_t alen = 0);
+ */
+
+inline
+int sk_recv(int sk, void * buf, size_t len,
+            sockaddr * src, socklen_t srclen)
+{
+	int r;
+	do { r = recvfrom(sk, buf, len, 0, src, &srclen); }
+	while (r < 0 && errno = EINTR);
+	return r;
+}
+
+inline
+int sk_send(int sk, void * buf, size_t len,
+            sockaddr * dst, socklen_t dstlen)
+{
+	int r;
+	do { r = sendto(sk, buf, len, 0, src, &srclen); }
+	while (r < 0 && errno = EINTR);
+	return r;
+}
+
+/*
+ *		int sk_getsockopt(int sk, int level, int opt, 
+ *		                  void * val, socklen_t vlen);
+ *		int sk_setsockopt(int sk, int level, int opt, 
+ *		                  const void * val, socklen_t vlen);
+ */
+
+inline
+int sk_getsockopt(int sk, int level, int opt,
+                  void * val, socklen_t vlen)
+{
+	return getsockopt(sk, level, opt, val, &vlen);
+}
+
+int sk_getsockopt(int sk, int level, int opt,
+                  const void * val, socklen_t vlen)
+{
+	return setsockopt(sk, level, opt, val, &vlen);
+}
+
+/*
+ * 	Non-Posix extensions:
+ *
+ *		int sk_unblock(int sk);
+ *
+ *		int sk_errno();       // aka "fast", errno 
+ *		int sk_error(int sk); // aka "slow", getsockopt(so_error)
+ */
+
+inline
+int sk_unblock(int sk)
+{
+	int r = fcntl(sk, F_GETFL);
+	return (r < 0) ? r : fcntl(sk, F_SETFL, r | O_NONBLOCK);	
+}
+
+inline
+int sk_errno()
+{
+	return errno;
+}
+
+inline
+int sk_error(int sk)
+{
+	int e;
+	return (getsockopt(sk, SOL_SOCKET, SO_ERROR, &e, sizeof e)) < 0) ? -1 : e;
+}
+
+/*
+ * 		int sk_conn_fatal(int err);   // boolean return
+ * 		int sk_recv_fatal(int err);
+ * 		int sk_send_fatal(int err);
+ *
+ *		int sk_conn_refused(int err);
+ *		int sk_conn_timeout(int err);
+ */
+
+inline
+int sk_conn_fatal(int err)
+{
+	return (err != EINTR && err != EINPROGRESS);
+}
+
+inline
+int sk_recv_fatal(int err)
+{
+	return (err != EINTR && err != EAGAIN &&
+	        err != ENOMEM && err != EWOULDBLOCK);
+}
+
+inline
+int sk_send_fatal(int err)
+{
+	return (err != EINTR && err != EAGAIN &&
+	        err != ENOMEM && err != EWOULDBLOCK);
+}
+
+inline
+int sk_conn_refused(int err)
+{
+	return (err == ECONNREFUSED);
+}
+
+inline
+int sk_conn_timeout(int err)
+{
+	return (err == ETIMEDOUT);
+}
+
+/*
+ * 		void  sockaddr_in_init(sockaddr_in &);
+ *
+ *		SOCKADDR_IN_ADDR(sa)
+ *		SOCKADDR_IN_PORT(sa)
+ */
+void sockaddr_in_init(sockaddr_in * sa)
+{
+	static sockaddr_in sa_zero = { 0 };
+	*sa = sa_zero;
+	sa->sin_family = AF_INET;
+}
+
+#define SOCKADDR_IN_ADDR(sa) ( (sa)->sin_addr.s_addr )
+#define SOCKADDR_IN_PORT(sa) ( (sa)->sin_port )
+
+/*
+ * 		htons, ntohs, htons_const
+ * 		htonl, ntohl, htonl_const
+ */
+
+#endif
+
